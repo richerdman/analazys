5c5640e9cd9e12470c0853cac6deb77ca624b7b8
richerdman <112634685+richerdman@users.noreply.github.com>
2024-09-09 13:34:26 +0000

    addin more files for a detailed look of the project

---
+++ app/app.py
@@ -0,0 +1,16 @@
+#import library
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+
+#Loading the social media posts
+
+
+
+#Initialize the sentiment Intensity analyzer
+sia = SentimentIntensityAnalyzer()
+
+
+#Analyze the sentiment of each post
